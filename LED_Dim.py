5c39eadbf4adbbfcc98c7c208d1f4a6b580ea11b
vmalombe <mbuvivictor@gmail.com>
2018-05-16 20:17:40 +0300

    Update LED_Dim.py

--- LED_Dim.py
+++ LED_Dim.py
@@ -35,4 +35,4 @@ except KeyboardInterrupt:
     white.stop()            # stop the white PWM output  
     red.stop()              # stop the red PWM output  
 finally:    
-GPIO.cleanup()          # clean up GPIO on CTRL+C exit  
+    GPIO.cleanup()          # clean up GPIO on CTRL+C exit  
